0949420b8d8ea97ce49c22464b747acdca8524de
Sam Bauch <sbauch@gmail.com>
2020-03-28 21:22:20 -0400

    issuer per ipi

--- models/identity_provider_instance.rb
+++ models/identity_provider_instance.rb
@@ -7,7 +7,8 @@ module Models
         'domain',
         'idp_cert',
         'idp_sso_target_url',
-        # 'assertion_consumer_service_url',
+      ).merge(
+        issuer: id,
       )
     end
   end
