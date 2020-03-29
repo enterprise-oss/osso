b656b2b06fb8518769983bbd65afc0ba6cbff029
Sam Bauch <sbauch@gmail.com>
2020-03-28 21:04:13 -0400

    slice

--- models/identity_provider_instance.rb
+++ models/identity_provider_instance.rb
@@ -3,7 +3,12 @@
 module Models
   class IdentityProviderInstance < ActiveRecord::Base
     def saml_options
-      attributes.slice('domain', 'idp_cert', 'idp_sso_target_url')
+      attributes.slice(
+        'domain',
+        'idp_cert',
+        'idp_sso_target_url',
+        'assertion_consumer_service_url',
+      )
     end
   end
 end
