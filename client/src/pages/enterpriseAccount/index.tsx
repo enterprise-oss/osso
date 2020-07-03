8f0e6bf5e69a6a78406f23087d4299c08da9f7ce
Sam Bauch <sbauch@gmail.com>
2020-07-03 16:28:44 -0400

    css, rm env

--- client/src/pages/enterpriseAccount/index.tsx
+++ client/src/pages/enterpriseAccount/index.tsx
@@ -11,45 +11,44 @@ import { InputProps } from './index.types';
 import { Card, Button, Select } from 'antd';
 
 export default (props: InputProps) => {
-  return null;
-  // const Option = Select.Option;
-  // const { data, loading } = useEnterpriseAccount(props.match.params.domain);
-  // const { providers } = useOssoFields();
-  // const [provider, setProvider] = useState<string>();
-  // const { createProvider } = createIdentityProvider();
+  const Option = Select.Option;
+  const { data, loading } = useEnterpriseAccount(props.match.params.domain);
+  const { providers } = useOssoFields();
+  const [provider, setProvider] = useState<string>();
+  const { createProvider } = createIdentityProvider();
 
-  // if (loading) return <div>Loading</div>;
-  // const { enterpriseAccount } = data;
+  if (loading) return <div>Loading</div>;
+  const { enterpriseAccount } = data;
 
-  // const onCreate = () => {
-  //   createProvider(enterpriseAccount?.id, provider);
-  // };
+  const onCreate = () => {
+    createProvider(enterpriseAccount?.id, provider);
+  };
 
-  // return (
-  //   <div>
-  //     {enterpriseAccount?.identityProviders?.map(
-  //       (provider: IdentityProvider) => (
-  //         <Card title={provider.service} key={provider.id}>
-  //           <SamlConfigForm id={provider.id} />
-  //         </Card>
-  //       ),
-  //     )}
+  return (
+    <div>
+      {enterpriseAccount?.identityProviders?.map(
+        (provider: IdentityProvider) => (
+          <Card title={provider.service} key={provider.id}>
+            <SamlConfigForm id={provider.id} />
+          </Card>
+        ),
+      )}
 
-  //     <Card title="Configure Identity Provider">
-  //       <p>
-  //         To add a new Identity provider for {enterpriseAccount?.name}, first
-  //         choose the Identity Provider Service
-  //       </p>
-  //       <Select
-  //         style={{ width: 120 }}
-  //         onChange={(value) => setProvider(value as string)}
-  //       >
-  //         {Object.values(providers).map((provider) => (
-  //           <Option value={provider.value}>{provider.label}</Option>
-  //         ))}
-  //       </Select>
-  //       <Button onClick={onCreate}>Get Started</Button>
-  //     </Card>
-  //   </div>
-  // );
+      <Card title="Configure Identity Provider">
+        <p>
+          To add a new Identity provider for {enterpriseAccount?.name}, first
+          choose the Identity Provider Service
+        </p>
+        <Select
+          style={{ width: 120 }}
+          onChange={(value) => setProvider(value as string)}
+        >
+          {Object.values(providers).map((provider) => (
+            <Option value={provider.value}>{provider.label}</Option>
+          ))}
+        </Select>
+        <Button onClick={onCreate}>Get Started</Button>
+      </Card>
+    </div>
+  );
 };
