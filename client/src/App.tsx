c5e6c8d43fd2d7cdba6bde7eb0f3108df8083c3b
Sam Bauch <sbauch@gmail.com>
2020-07-02 22:18:41 -0400

    dev

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -22,41 +22,8 @@ import EnterpriseAccounts from './pages/enterpriseAccounts/index';
 function App() {
   const location = useLocation();
   console.log(location.pathname.split('/')[1]);
-  return (
-    // <OssoProvider>
-    <Layout>
-      <Sider width={220}>
-        {/* <Brand className="brand" /> */}
-        <Menu mode="inline" selectedKeys={[location.pathname.split('/')[1]]}>
-          <Menu.Item key="">
-            <NavLink to="/">Home</NavLink>
-          </Menu.Item>
-          <Menu.Item key="enterprise">
-            <NavLink to="/enterprise">Enterprise Customers</NavLink>
-          </Menu.Item>
-          <Menu.Item key="/config">
-            <NavLink to="/config">Developer Configuration</NavLink>
-          </Menu.Item>
-        </Menu>
-      </Sider>
-      <Layout>
-        <Header>Header HMR</Header>
-        <Content className="main">
-          <Redirect from="/" to="/enterprise" />
-          <Switch>
-            <Route exact path="/enterprise">
-              <EnterpriseAccounts />
-            </Route>
-            <Route path="/enterprise/:domain" component={EnterpriseAccount} />
-            <Route exact path="/config">
-              <DeveloperConfig />
-            </Route>
-          </Switch>
-        </Content>
-        <Footer>Footer</Footer>
-      </Layout>
-    </Layout>
-    // </OssoProvider>
+  return (<p>hey</p>
+
   );
 }
 
