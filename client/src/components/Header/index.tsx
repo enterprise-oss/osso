b630606c47a144f2bd2d268370608e32e7f9d446
Sam Bauch <sbauch@gmail.com>
2020-07-13 16:01:52 -0400

    animate header

--- client/src/components/Header/index.tsx
+++ client/src/components/Header/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Layout } from 'antd';
 import { useHistory, useLocation } from 'react-router-dom';
 import { useEnterpriseAccount } from '@enterprise-oss/osso';
@@ -9,14 +9,30 @@ import classnames from 'classnames';
 export default function Header() {
   const location = useLocation();
   const history = useHistory();
-  const pathArray = location.pathname.split('/').filter(Boolean);
-  const nested = pathArray.length > 1;
+  const [headerState, setHeaderState] = useState({
+    backPath: '/',
+    Inner: null,
+    nested: false,
+  });
+
+  useEffect(() => {
+    const pathArray = location.pathname.split('/').filter(Boolean);
+    const nested = pathArray.length > 1;
+
+    setHeaderState({
+      backPath: '/' + pathArray[0],
+      Inner: <EnterpriseAccountName domain={pathArray[1]} />,
+      nested,
+    });
+  }, [location.pathname]);
+
+  const { nested, backPath, Inner } = headerState;
 
   return (
     <Layout.Header className={styles.header}>
       <div className={styles.breadcrumbs}>
         <h1
-          onClick={() => nested && history.push(`/${pathArray[0]}`)}
+          onClick={() => nested && history.push(backPath)}
           className={classnames(styles.breadcrumb, {
             [styles.breadcrumbRoot]: nested,
           })}
@@ -29,7 +45,7 @@ export default function Header() {
         {nested && (
           <>
             <Separator />
-            <EnterpriseAccountName domain={pathArray[1]} />
+            {Inner}
           </>
         )}
       </div>
