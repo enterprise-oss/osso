c914bdf8486fa16c35fbf48087f03c59dbd6ec83
Sam Bauch <sbauch@gmail.com>
2020-07-13 08:36:47 -0400

    hmm

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,9 +1,13 @@
-import jwt from 'jsonwebtoken';
+import jwt from "jsonwebtoken";
 
 Cypress.Commands.add("login", (email, scope) => {
-  const token = jwt.sign({
-    email, scope
-  }, Cypress.env('JWT_HMAC_SECRET'));
+  const token = jwt.sign(
+    {
+      email,
+      scope,
+    },
+    Cypress.env("JWT_HMAC_SECRET")
+  );
 
-  cy.visit(`/admin?admin_token=${token}`)
-})
+  cy.visit(`/admin?admin_token=${token}`);
+});
