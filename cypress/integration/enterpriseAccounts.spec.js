8dd5125094929c13652da5dbcab0ccbddac8f2bb
Sam Bauch <sbauch@gmail.com>
2020-07-16 02:18:47 -0400

    target link

--- cypress/integration/enterpriseAccounts.spec.js
+++ cypress/integration/enterpriseAccounts.spec.js
@@ -7,10 +7,14 @@ describe("EnterpriseAccounts", () => {
     it("can create an enterprise account", () => {
       cy.visit("/admin/enterprise");
       cy.get("button#addNew").click();
+
       cy.get("input#domain").type("foo.com");
       cy.get("input#name").type("Foo");
+
       cy.contains("OK").click();
-      cy.get("table tr").last().get("td").contains("Foo").click();
+
+      cy.get("table tr").last().get("td > a").contains("Foo").click();
+
       cy.url().should("include", "/enterprise/foo.com");
     });
   });
