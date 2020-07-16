4e4b92475028b26d5384b3bf448716588a5471c8
Sam Bauch <sbauch@gmail.com>
2020-07-16 02:26:13 -0400

    faker data

--- cypress/integration/enterpriseAccounts.spec.js
+++ cypress/integration/enterpriseAccounts.spec.js
@@ -1,21 +1,28 @@
+import faker from "faker";
+
 describe("EnterpriseAccounts", () => {
   describe("with an Admin user", () => {
     before(() => {
-      cy.login("admin@saas.com", "admin");
+      cy.login("admin@example.com", "admin");
     });
 
     it("can create an enterprise account", () => {
+      const company = faker.company.companyName();
+      const domain = faker.internet.domainName();
+
       cy.visit("/admin/enterprise");
       cy.get("button#addNew").click();
 
-      cy.get("input#domain").type("foo.com");
-      cy.get("input#name").type("Foo");
+      cy.get("input#domain").type(domain);
+      cy.get("input#name").type(company);
 
       cy.contains("OK").click();
 
-      cy.get("table tr").last().get("td > a").contains("Foo").click();
+      cy.get("table tr").last().get("td > a").contains(company).click();
 
-      cy.url().should("include", "/enterprise/foo.com");
+      cy.url().should("include", `/enterprise/${domain}`);
+
+      cy.get("button").contains("Add New").click();
     });
   });
 });
