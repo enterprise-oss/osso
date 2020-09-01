2875f755045cae42c26e07d18d502dd851c51491
Sam Bauch <sbauch@gmail.com>
2020-09-01 16:13:43 -0400

    Add Cypress tests for OAuth Clients and Identity Providers

---
+++ cypress/integration/identityProvider.spec.js
@@ -0,0 +1,60 @@
+describe("Customer Identity Providers", () => {
+  describe("an Admin user", () => {
+    beforeEach(() => {
+      cy.login("admin@example.com", "admin");
+      cy.visit("/admin/enterprise/customer.com");
+    });
+
+    it("can add a new Okta provider", () => {
+      cy.contains("Actions").click();
+      cy.contains("Add new IDP").click();
+
+      cy.contains("Okta").click();
+      cy.contains("Next").click();
+
+      cy.get("input#osso-okta-acs-url")
+        .invoke("val")
+        .then((acsUrl) => {
+          expect(acsUrl).to.match(/.*\/auth\/saml\/.*\/callback/);
+        });
+
+      cy.get("input#osso-okta-entity-id")
+        .invoke("val")
+        .then((entityId) => {
+          expect(entityId).to.eq("customer.com");
+        });
+    });
+
+    it("can add a new Azure provider", () => {
+      cy.contains("Actions").click();
+      cy.contains("Add new IDP").click();
+
+      cy.contains("Azure").click();
+      cy.contains("Next").click();
+
+      cy.get("input#osso-azure-acs-url")
+        .invoke("val")
+        .then((acsUrl) => {
+          expect(acsUrl).to.match(
+            /http:\/\/localhost:9292\/auth\/saml\/.*\/callback/
+          );
+        });
+
+      cy.get("input#osso-azure-entity-id")
+        .invoke("val")
+        .then((entityId) => {
+          expect(entityId).to.eq("customer.com");
+        });
+    });
+
+    xit("can download the PDF setup docs", () => {
+      cy.task("allowDownloads");
+
+      cy.contains("Download").click();
+    });
+
+    it("can complete configuration", () => {
+      cy.contains("Complete").click();
+    });
+  });
+});
---
+++ cypress/integration/oauthClient.spec.js
@@ -0,0 +1,48 @@
+describe("Oauth Client", () => {
+  describe("with an Admin user", () => {
+    beforeEach(() => {
+      cy.login("admin@example.com", "admin");
+      cy.visit("/admin/config");
+      cy.get("table tr").get("td > a").last().click();
+    });
+
+    it("can regenerate credentials", () => {
+      cy.get("input#osso-client-id")
+        .invoke("val")
+        .then((val1) => {
+          cy.contains("Regenerate").click();
+
+          cy.get("input#osso-client-id")
+            .invoke("val")
+            .should((val2) => {
+              expect(val1).not.to.eq(val2);
+            });
+        });
+    });
+
+    it("can add a redirect URI", () => {
+      const callback = "https://foo.com/callback";
+      cy.contains("Add new redirect").click();
+      cy.get("button.ant-btn-icon-only").click();
+
+      cy.get("div#uri-0").find("input").type(callback);
+
+      cy.contains("Done").click();
+
+      cy.contains(callback);
+    });
+
+    it("can duplicate the oauth client", () => {
+      const url = cy.url();
+      cy.contains("Actions").click();
+      cy.contains("Duplicate").click();
+      expect(cy.url()).to.not.equal(url);
+    });
+
+    it("can delete the client", () => {
+      cy.contains("Actions").click();
+      cy.contains("Delete").click();
+      cy.url().should("match", /\/admin\/config$/);
+    });
+  });
+});
