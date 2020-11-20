67b6640d9500474ae312ff0d7f3cf57dc0d7f990
Sam Bauch <sbauch@gmail.com>
2020-11-19 19:09:36 -0500

    uncomment

--- .aws/modules/rds/main.tf
+++ .aws/modules/rds/main.tf
@@ -81,7 +81,7 @@ resource "aws_db_instance" "rds" {
   multi_az               = "${var.multi_az}"
   name                   = "${var.database_name}"
   username               = "${local.db_creds.username}"
-  password               = "${local.db_creds.username}"
+  password               = "${local.db_creds.password}"
   db_subnet_group_name   = "${aws_db_subnet_group.rds_subnet_group.id}"
   vpc_security_group_ids = ["${aws_security_group.rds_sg.id}"]
   skip_final_snapshot    = true
