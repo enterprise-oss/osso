d5d2e0594ab7127079a3becfe5dad824ccddd015
Sam Bauch <sbauch@gmail.com>
2020-11-24 16:40:32 -0500

    SESSION_SECRET for cypress rake

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -2,9 +2,9 @@ import "./commands";
 
 before(() => {
   cy.exec(
-    "BASE_URL=http://localhost:9292 RACK_ENV=test bundle exec rake db:migrate"
+    "BASE_URL=http://localhost:9292 RACK_ENV=test SESSION_SECRET=secret bundle exec rake db:migrate"
   );
   cy.exec(
-    "BASE_URL=http://localhost:9292 RACK_ENV=test bundle exec rake db:seed:replant"
+    "BASE_URL=http://localhost:9292 RACK_ENV=test SESSION_SECRET=secret bundle exec rake db:seed:replant"
   );
 });
