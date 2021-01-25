531884489ccf65f75dbab9ad6484aaf6934663c8
Sam Bauch <sbauch@gmail.com>
2021-01-25 12:45:26 -0500

    add files

---
+++ cypress/integration/saml.spec.js
@@ -0,0 +1,21 @@
+describe("SAML Logins", () => {
+  it("can perform SP initiated login via hosted login and Mock IDP", () => {
+    cy.visit("/oauth/authorize", {
+      qs: {
+        client_id: "demo-client-id",
+        redirect_uri: "http://localhost:9292/health",
+        response_type: "code",
+      },
+    });
+
+    cy.get("input#email-input").type("user@example.com");
+    cy.get("input#hosted-login-button").click();
+
+    cy.get("input[name='email']").type("user@example.com");
+    cy.get("input[name='password']").type("password");
+
+    cy.get("input#hosted-login-button").click();
+
+    cy.contains("ok");
+  });
+});
